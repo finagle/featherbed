cfcd5fda7a9458eadd19f2f96bcd728e76e0469b
Brandon Hudgeons <brandon@atsoft.at>
2016-03-10 15:50:34 -0600

    Default encoder now excludes Option.None instead of "key":null

--- featherbed-circe/src/main/scala/featherbed/circe/package.scala
+++ featherbed-circe/src/main/scala/featherbed/circe/package.scala
@@ -10,9 +10,11 @@ import shapeless.Witness
 
 package object circe {
 
+  private val printer = Printer.noSpaces.copy(dropNullKeys = true)
+
   implicit def circeEncoder[A : Encoder] : content.Encoder[A, Witness.`"application/json"`.T] =
     content.Encoder.of("application/json") {
-      (value: A, charset: Charset) => content.Encoder.encodeString(value.asJson.toString, charset)
+      (value: A, charset: Charset) => content.Encoder.encodeString(printer.pretty(value.asJson), charset)
     }
 
   implicit def circeDecoder[A : Decoder] : content.Decoder.Aux[Witness.`"application/json"`.T, A] =
